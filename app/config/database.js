1770ef0d4efd8a5a3379b68dd609aabfa55dc99e
Rodrigo Souza <rdrg.rcs1997@gmail.com>
2019-10-21 16:49:34 -0300

    adicionei uma conexção de banco atlas no projeto.

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/ProjetoBackend', { useNewUrlParser: true, useUnifiedTopology: true })
+const uri = "mongodb+srv://rodrigoconcrete:Rodrigo549393@cluster0-jr8yr.mongodb.net/ProjetoBackend";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise
 
