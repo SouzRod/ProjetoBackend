cd9cf8faa7c9eb3c1ca48e9475a52562b1120bfe
Rodrigo Souza <rdrg.rcs1997@gmail.com>
2019-10-22 14:44:45 -0300

    doc database modificado.

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@${process.env.ATLAS_CONNECTION}/ProjetoBackend`;
+let uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@${process.env.ATLAS_CONNECTION}/ProjetoBackend`;
+if(process.env.NODE_ENV === 'local') {
+    uri = 'mongodb://localhost/ProjetoBackend'
+} 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.set('useCreateIndex', true);
 
